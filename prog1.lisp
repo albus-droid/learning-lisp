28ac4cb2ec0f84487c5913f97e12be08ddd9f3f7
albus <albinbabuvarghese20@gmail.com>
2023-01-12 22:44:31 +0530

    First 3 programs I created
    I'm following this [video](https://www.youtube.com/watch?v=ymSq4wHrqyU).

---
+++ prog1.lisp
@@ -0,0 +1,21 @@
+;;; Comment
+
+;; Comment
+
+#||
+Multiline
+||#
+
+(format t "~%Hello World~%")
+
+(print "What's your name")
+
+(defvar *name* (read))
+
+(defun hello-you (*name*)
+  (format t "Hello ~a! ~%" *name*)
+)
+
+(setq *print-case* :capitalize)
+
+(hello-you *name*)
